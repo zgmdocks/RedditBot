4601def756e23b44052a65c0e09fc09a2abe8769
Zach <z.dockstader@gmail.com>
2016-12-20 11:38:06 -0500

    fixed problem where couldn't find parent

--- replyBot.py
+++ replyBot.py
@@ -8,7 +8,9 @@ reddit = praw.Reddit(client_id='PUtf-2W1mJ2cvQ',
         password='redditbot',
         username='umm_totally_real')
 
-for reply in reddit.inbox.comment_replies():
-    if 'wrong thread' in reply.body:
-        print(reply.body)
-        comment.delete()
+for comment in reddit.redditor('umm_totally_real').comments.new(limit=25):
+    comment.refresh()
+    replies = comment.replies
+    for reply in replies:
+        if 'wrong thread' in reply.body:
+            comment.delete()
